8d870be4734c76d68fa68fb160a944c6071bbba5
xuqinkun <xu_qinkun@163.com>
2020-12-15 15:04:15 +0800

    Add parameters from console

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from model import TransformerEncoder
 from model import MaskedSoftmaxCELoss
 from model import TransformerDecoder
 import torch
+import argparse
 from d2l import torch as d2l
 from torch import nn
 from load import *
@@ -116,10 +117,35 @@ def predict(model, src_sentence, src_vocab, tgt_vocab, num_steps,
     return ' '.join(tgt_vocab.to_tokens(output_seq))
 
 
+def parse():
+    parser = argparse.ArgumentParser(description='Attention Seq2Seq Chatbot')
+    # parser.add_argument('-tr', '--train', help='Train the model with corpus')
+    # parser.add_argument('-te', '--test', help='Test the saved model')
+    # parser.add_argument('-l', '--load', help='Load the model and train')
+    # parser.add_argument('-c', '--corpus', help='Test the saved model with vocabulary of the corpus')
+    # parser.add_argument('-r', '--reverse', action='store_true', help='Reverse the input sequence')
+    # parser.add_argument('-f', '--filter', action='store_true', help='Filter to small training data set')
+    # parser.add_argument('-i', '--input', action='store_true', help='Test the model by input the sentence')
+    parser.add_argument('-it', '--iteration', type=int, default=10000, help='Train the model with it iterations')
+    # parser.add_argument('-p', '--print', type=int, default=100, help='Print every p iterations')
+    # parser.add_argument('-b', '--batch_size', type=int, default=64, help='Batch size')
+    # parser.add_argument('-la', '--layer', type=int, default=1, help='Number of layers in encoder and decoder')
+    # parser.add_argument('-hi', '--hidden', type=int, default=256, help='Hidden size in encoder and decoder')
+    # parser.add_argument('-be', '--beam', type=int, default=1, help='Hidden size in encoder and decoder')
+    # parser.add_argument('-s', '--save', type=int, default=500, help='Save every s iterations')
+    # parser.add_argument('-lr', '--learning_rate', type=float, default=0.01, help='Learning rate')
+    # parser.add_argument('-d', '--dropout', type=float, default=0.1, help='Dropout probability for rnn and dropout layers')
+
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == '__main__':
+    args = parse()
+
     embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
     batch_size, num_steps = 64, 100
-    data_size = 100
+    data_size = args.iteration
     lr, num_epochs, device = 0.005, 1, d2l.try_gpu()
 
     train_iter, src_vocab, tgt_vocab = load_data("data/movie_subtitles.txt", data_size, num_steps, batch_size)
